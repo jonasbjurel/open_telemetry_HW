f5d9a6a802d1628e28a0ebb4e299067274860c8f
jonasbjurel@hotmail.com <jonasbjurel@hotmail.com>
2020-01-25 18:11:11 +0100

    Fixed OFT001v1 BIST GPS reading issues

---
+++ pcb/sensors/Open_Telemetry_digitalSensorShield/BIST/OFT001v1_BIST/OFT001v1_BIST/Debug/BLE/BLEValue.cpp.d
@@ -0,0 +1,11 @@
+C:\Users\uabjonb\AppData\Local\Temp\VMBuilds\OFT001v1_BIST\esp32_featheresp32\Debug\BLE\BLEValue.cpp.o: \
+ C:\Users\uabjonb\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.3\libraries\BLE\src\BLEValue.cpp \
+ C:\Users\uabjonb\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.3/tools/sdk/include/config/sdkconfig.h \
+ C:\Users\uabjonb\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.3\libraries\BLE\src\BLEValue.h \
+ C:\Users\uabjonb\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.3\cores\esp32/esp32-hal-log.h \
+ C:\Users\uabjonb\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.3/tools/sdk/include/log/esp_log.h \
+ C:\Users\uabjonb\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.3/tools/sdk/include/esp32/rom/ets_sys.h \
+ C:\Users\uabjonb\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.3/tools/sdk/include/soc/soc/soc.h \
+ C:\Users\uabjonb\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.3/tools/sdk/include/esp32/esp_assert.h \
+ C:\Users\uabjonb\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.3/tools/sdk/include/newlib/assert.h \
+ C:\Users\uabjonb\AppData\Local\arduino15\packages\esp32\hardware\esp32\1.0.3/tools/sdk/include/log/esp_log_internal.h
---
+++ pcb/sensors/Open_Telemetry_digitalSensorShield/BIST/OFT001v1_BIST/OFT001v1_BIST/Debug/VM_DBG/VM_mem_check_sam.cpp.d
@@ -0,0 +1,2 @@
+C:\Users\uabjonb\AppData\Local\Temp\VMBuilds\OFT001v1_BIST\esp32_featheresp32\Debug\VM_DBG\VM_mem_check_sam.cpp.o: \
+ c:\program\ files\ (x86)\microsoft\ visual\ studio\2019\community\common7\ide\extensions\3pjlccdc.dkb\Micro\ Platforms\default\debuggers\VM_DBG\VM_mem_check_sam.cpp
